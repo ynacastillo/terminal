e8ad018a8b7050eaf3e73e6f69e7f992a97bb34d
Leonard Hecker <lhecker@microsoft.com>
2023-09-02 17:55:40 +0200

    wip

--- src/inc/til/ticket_lock.h
+++ src/inc/til/ticket_lock.h
@@ -11,7 +11,7 @@ namespace til
     //
     // Compared to a SRWLOCK this implementation is significantly more unsafe to use:
     // Forgetting to call unlock or calling unlock more than once, will lead to deadlocks,
-    // as _now_serving will remain out of sync with _next_ticket and prevent any further lockings.
+    // and there are no runtime checks or tools to test for this.
     //
     // I recommend to use the following with this class:
     // * A low number of concurrent accesses (this lock doesn't scale well beyond 2 threads)
@@ -22,26 +22,30 @@ namespace til
         void lock() noexcept
         {
             const auto ticket = _next_ticket.fetch_add(1, std::memory_order_relaxed);
+            const auto current = _now_serving.load(std::memory_order_acquire);
 
-            for (;;)
+            if (ticket != current)
             {
-                const auto current = _now_serving.load(std::memory_order_acquire);
-                if (current == ticket)
-                {
-                    break;
-                }
-
-                til::atomic_wait(_now_serving, current);
+                _lockSlow(ticket, current);
             }
         }
 
         void unlock() noexcept
         {
             _now_serving.fetch_add(1, std::memory_order_release);
-            til::atomic_notify_all(_now_serving);
+            til::atomic_notify_one(_now_serving);
         }
 
     private:
+        __declspec(noinline) void _lockSlow(uint32_t ticket, uint32_t current) const noexcept
+        {
+            do
+            {
+                til::atomic_wait(_now_serving, current);
+                current = _now_serving.load(std::memory_order_acquire);
+            } while (current != ticket);
+        }
+
         // You may be inclined to add alignas(std::hardware_destructive_interference_size)
         // here to force the two atomics on separate cache lines, but I suggest to carefully
         // benchmark such a change. Since this ticket_lock is primarily used to synchronize
@@ -54,6 +58,26 @@ namespace til
         std::atomic<uint32_t> _now_serving{ 0 };
     };
 
+    // This class builds on top of fair_mutex to allow for recursive locking.
+    //
+    // Implementing a mutex that has recursion built in would be slightly more effective, but...
+    // Relying on recursive/reentrant mutexes is a universally bad idea.
+    //
+    // They historically resulted from improvements in encapsulation, to bundle data and behavior and make both
+    // opaque from the outside (callers). This resulted in design where entire classes/modules can be considered
+    // thread-safe, because the thread-safety of not just its data, but also all of its functionality is insured by
+    // its internal implementation. As the code grows and ages, this results in problems where you inadvertently call
+    // your own public implementation that reacquires the mutex, instead of an internal implementation that doesn't.
+    // Now, reentrant locking looks rather useful.
+    //
+    // A common example for this is the Win32 HWND message loop where reentrant mutexes are often used, because calling
+    // a HWND based function (including DefWindowProc) can result in synchronous calls back into the message loop.
+    // This may also be a good example for why this is so seldom the case as you can call MsgWaitForMultipleObjectsEx to
+    // wait for window messages, then acquire the mutex, run a PeekMessage/DispatchMessage loop and then unlock again.
+    // It could be argued that this is worse because it holds the lock for longer, but this is also a good example for
+    // why reentrancy doesn't actually help here. There's no guarantee how long any external call takes,
+    // in particular system calls. Even a simple malloc() may block indefinitely.
+    // Reentrancy doesn't solve this, but it does buy a false sense of security.
     struct recursive_ticket_lock
     {
         struct recursive_ticket_lock_suspension
@@ -148,7 +172,7 @@ namespace til
 
     private:
         ticket_lock _lock;
-        std::atomic<uint32_t> _owner = 0;
+        alignas(std::hardware_destructive_interference_size) std::atomic<uint32_t> _owner = 0;
         uint32_t _recursion = 0;
     };
 
